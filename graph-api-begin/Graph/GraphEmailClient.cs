8ac317987839028102f6a9fda19676383451e3de
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2023-12-20 15:32:09 +0700

    Add files via upload

---
+++ graph-api-begin/Graph/GraphEmailClient.cs
@@ -0,0 +1,57 @@
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace DotNetCoreRazor_MSGraph.Graph
+{
+    public class GraphEmailClient
+    {
+        private readonly ILogger<GraphEmailClient> _logger = null;
+        private readonly GraphServiceClient _graphServiceClient = null;
+
+        public GraphEmailClient(
+
+            ILogger<GraphEmailClient> logger,
+            GraphServiceClient graphServiceClient)
+        {
+            _logger = logger;
+            _graphServiceClient = graphServiceClient;
+        }
+        )
+        {
+            
+        }
+
+        public async Task<IEnumerable<Message>> GetUserMessages()
+        {
+            // Remove this code
+            return await Task.FromResult<IEnumerable<Message>>(null);
+        }
+
+        public async Task<(IEnumerable<Message> Messages, string NextLink)> GetUserMessagesPage(
+            string nextPageLink = null, int top = 10)
+        {
+            // Remove this code
+            return await Task.FromResult<
+                (IEnumerable<Message> Messages, string NextLink)>((Messages:null, NextLink:null));
+        }
+
+        private string GetNextLink(IUserMessagesCollectionPage pagedMessages) {
+            if (pagedMessages.NextPageRequest != null)
+            {
+                // Get the URL for the next batch of records
+                return pagedMessages.NextPageRequest.GetHttpRequestMessage().RequestUri?.OriginalString;
+            }
+            return null;
+        }
+
+    }
+}
